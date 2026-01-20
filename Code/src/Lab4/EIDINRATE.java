0ccad49d6953f20db78f9b2a4f7e6351225944f1
Nguyễn Hữu Nhân <hngaming0947620550@gmail.com>
2026-01-20 16:19:52 +0700

    EIDINRATE

---
+++ Code/src/Lab4/EIDINRATE.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class EIDINRATE {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt();
+        while (T-- > 0) {
+            double N = sc.nextDouble();
+            double L = sc.nextDouble();
+            double M = sc.nextDouble();
+            int years = 0;
+            double rate = 1 + L / 100.0;
+            double amount = N;
+            while (amount < M) {
+                amount *= rate;
+                years++;
+            }
+            System.out.println(years);
+        }
+
+    }
+}
