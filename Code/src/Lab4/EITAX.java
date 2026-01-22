a483095b80f6a5d87abdc98a5c50cd4154fd35fb
Nguyễn Hữu Nhân <hngaming0947620550@gmail.com>
2026-01-22 16:17:01 +0700

    add EITAX and EIUSAVING1

---
+++ Code/src/Lab4/EITAX.java
@@ -0,0 +1,22 @@
+package Lab4;
+
+import java.util.Scanner;
+
+public class EITAX {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        long[] levels = {0, 5000000, 10000000, 18000000, 32000000, 52000000, 80000000, Long.MAX_VALUE / 2};
+        double[] rates = {0.05, 0.10, 0.15, 0.20, 0.25, 0.30, 0.35};
+        
+        long salary = sc.nextLong();
+        long taxable = salary - 9000000;
+        double total = 0;
+
+        for (int i = 0; i < rates.length && taxable > 0; i++) {
+            long salaryIn = Math.min(taxable, levels[i + 1] - levels[i]);
+            total += salaryIn * rates[i];
+            taxable -= salaryIn;
+        }
+        System.out.println(Math.round(total));
+    }
+}
---
+++ Code/src/Lab4/EIUSAVING1.java
@@ -0,0 +1,22 @@
+package Lab4;
+
+import java.util.Scanner;
+
+public class EIUSAVING1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        double[] rate = { 3.9, 3.92, 3.95, 3.99, 4.04, 5.54, 5.72, 5.92, 6.14, 6.38, 6.64, 6.92 };
+        double total = n;
+        int year = m / 12;
+        int month = m % 12; 
+        if (year > 0)
+            total *= Math.pow(1 + rate[11] / 100, year);
+        if (month > 0)
+            total *= 1 + rate[month - 1] / 12 / 100 * month;
+        System.out.println(Math.round(total));
+
+    }
+
+}
